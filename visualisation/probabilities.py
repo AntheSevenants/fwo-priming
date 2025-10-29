b32752b9ea867cb0f7d19afa736099f2d77ae5dc
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-10-29 15:43:48 +0100

    Add plot for plotting construction preferences per agent over time

--- visualisation/probabilities.py
+++ visualisation/probabilities.py
@@ -9,3 +9,11 @@ def plot_ctx_probs_mean(model, ax=None, disable_title=False):
         ax=ax,
         disable_title=disable_title,
     )
+
+def plot_ctx_probs_per_agent(model, disable_title=False):
+    return visualisation.core.plot_ratio_pass(
+        model,
+        "ctx_probs_per_agent",
+        title="Evolution of relative preference per agent",
+        disable_title=disable_title
+    )
