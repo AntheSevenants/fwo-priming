e3817a4c966a3d2e4cf9b00d8cf69d36233b340f
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-10-29 15:39:54 +0100

    Add visualisation for time series for each separate agent

--- visualisation/core.py
+++ visualisation/core.py
@@ -1,18 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-COLOURS = [ "blue", "orange", "green", "red", "purple" ]
+COLOURS = ["blue", "orange", "green", "red", "purple"]
+
+
+def formatter(x, pos, scale=100):
+    del pos
+    return str(int(x * scale))
+
 
 def check_ax(ax=None, disable_title=False):
     if ax is None:
         fig, ax = plt.subplots(figsize=(6, 4))
-    
+
     fig = ax.get_figure()
-    
+
     if disable_title:
         fig.tight_layout()
 
     return fig, ax
+
 
 def plot_ratio(model, attribute, ax=None, title=None, disable_title=False):
     df = model.datacollector.get_model_vars_dataframe()
@@ -21,10 +28,45 @@ def plot_ratio(model, attribute, ax=None, title=None, disable_title=False):
 
     matrix = np.stack(df[attribute])
     for i in range(matrix.shape[1]):
-        ax.plot(matrix[:,i], color=COLOURS[i])
+        ax.plot(matrix[:, i], color=COLOURS[i])
 
     if title is not None and not disable_title:
         ax.set_title(title)
 
+
 def plot_ratio_pass(model, attribute, ax=None, title=None, disable_title=False):
-    pass
+    df = model.datacollector.get_model_vars_dataframe()
+
+    if ax is None:
+        fig, axes = plt.subplots(
+            nrows=1, ncols=model.num_agents, figsize=(15, 10), sharey=True
+        )
+    else:
+        raise ValueError(
+            "Cannot do mosaic plots for this graph type. Please do not pass an ax."
+        )
+
+    matrix = np.stack(df[attribute])
+    num_steps = matrix.shape[0]
+    time_steps = np.arange(num_steps)
+    # Vertical baseline which shows 0.5
+    baseline = np.full(num_steps, 0.5)
+
+    for i, ax in enumerate(axes):
+        # Plot 0.5 baselien first
+        ax.plot(baseline, time_steps, color="gray", alpha=0.1)
+
+        ax.plot(matrix[:, i, 0], time_steps, color="blue")
+        ax.set_title(f"{i + 1}")
+        ax.set_xticks([])
+        # ax.set_xlabel('Construction 0 usage')
+        ax.invert_yaxis()  # So time increases upward
+        ax.grid(True)
+
+        # Disable ugly boxes
+        for spine in ax.spines.values():
+            spine.set_visible(False)
+
+    axes[0].set_ylabel("Time steps in the simulation")
+
+    return ax
