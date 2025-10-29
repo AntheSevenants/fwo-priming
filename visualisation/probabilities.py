6ce64a5299946a3b2b9b8e5aa9f805f2807bba5c
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-10-29 14:52:02 +0100

    Add more options to probs mean viz function

--- visualisation/probabilities.py
+++ visualisation/probabilities.py
@@ -1,7 +1,11 @@
 import visualisation.core
 
 
-def plot_ctx_probs_mean(model, ax=None):
+def plot_ctx_probs_mean(model, ax=None, disable_title=False):
     return visualisation.core.plot_ratio(
-        model, "ctx_probs_mean", title="Mean probability across agents", ax=ax
+        model,
+        "ctx_probs_mean",
+        title="Mean probability across agents",
+        ax=ax,
+        disable_title=disable_title,
     )
