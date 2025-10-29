bf54c34d725d16742685c2aa0a795ac7acdc2e00
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-10-29 14:49:33 +0100

    Make core visualisation code

--- visualisation/core.py
+++ visualisation/core.py
@@ -3,9 +3,7 @@ import numpy as np
 
 COLOURS = [ "blue", "orange", "green", "red", "purple" ]
 
-def plot_ratio(model, attribute, ax=None, title=None, disable_title=False):
-    df = model.datacollector.get_model_vars_dataframe()
-
+def check_ax(ax=None, disable_title=False):
     if ax is None:
         fig, ax = plt.subplots(figsize=(6, 4))
     
@@ -14,9 +12,19 @@ def plot_ratio(model, attribute, ax=None, title=None, disable_title=False):
     if disable_title:
         fig.tight_layout()
 
+    return fig, ax
+
+def plot_ratio(model, attribute, ax=None, title=None, disable_title=False):
+    df = model.datacollector.get_model_vars_dataframe()
+
+    fig, ax = check_ax(ax, disable_title)
+
     matrix = np.stack(df[attribute])
     for i in range(matrix.shape[1]):
         ax.plot(matrix[:,i], color=COLOURS[i])
 
     if title is not None and not disable_title:
         ax.set_title(title)
+
+def plot_ratio_pass(model, attribute, ax=None, title=None, disable_title=False):
+    pass
