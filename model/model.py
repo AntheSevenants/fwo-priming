b4b54998b8e0832704b5c901564c2b7d122caa08
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-02-04 14:22:58 +0100

    Formatting

--- model/model.py
+++ model/model.py
@@ -14,10 +14,7 @@ from typing import List, Optional
 class PrimingModel(mesa.Model):
     """A model of syntactic priming"""
 
-    def __init__(
-        self,
-        params: model.model_defaults.Parameters
-    ):
+    def __init__(self, params: model.model_defaults.Parameters):
         # Load parameters
         self.params = params
 
@@ -26,18 +23,41 @@ class PrimingModel(mesa.Model):
         self.nprandom = np.random.default_rng(self.params.seed)
 
         # Agents
-        agents = model.agent.PrimingAgent.create_agents(model=self, n=self.params.num_agents)
+        agents = model.agent.PrimingAgent.create_agents(
+            model=self, n=self.params.num_agents
+        )
 
         # Model data collection
         self.tracker = model.tracker.Tracker(self)
 
         model_reporters = {
-            "ctx_probs_per_agent": lambda model: model.tracker.get_property_per_agent("activation"),
-            "starting_probs_per_agent": lambda model: model.tracker.get_property_per_agent("starting_base_rate"),
-            "ctx_probs_mean": lambda model: model.tracker.get_property_mean_across_agents("activation"),
-            "ctx_entropy_per_agent": lambda model: model.tracker.get_property_per_agent("entropy", index=-1),
-            "ctx_entropy_mean": lambda model: model.tracker.get_property_mean_across_agents("entropy", index=-1),
-            "consensus_reached": lambda model: False if len(model.datacollector.model_vars["ctx_probs_mean"]) == 0 else (np.any(np.isclose(model.datacollector.model_vars["ctx_probs_mean"][-1], 1)) is np.True_)
+            "ctx_probs_per_agent": lambda model: model.tracker.get_property_per_agent(
+                "activation"
+            ),
+            "starting_probs_per_agent": lambda model: model.tracker.get_property_per_agent(
+                "starting_base_rate"
+            ),
+            "ctx_probs_mean": lambda model: model.tracker.get_property_mean_across_agents(
+                "activation"
+            ),
+            "ctx_entropy_per_agent": lambda model: model.tracker.get_property_per_agent(
+                "entropy", index=-1
+            ),
+            "ctx_entropy_mean": lambda model: model.tracker.get_property_mean_across_agents(
+                "entropy", index=-1
+            ),
+            "consensus_reached": lambda model: (
+                False
+                if len(model.datacollector.model_vars["ctx_probs_mean"]) == 0
+                else (
+                    np.any(
+                        np.isclose(
+                            model.datacollector.model_vars["ctx_probs_mean"][-1], 1
+                        )
+                    )
+                    is np.True_
+                )
+            ),
         }
         self.datacollector = mesa.DataCollector(model_reporters=model_reporters)
         self.datacollector.collect(self)
@@ -46,5 +66,8 @@ class PrimingModel(mesa.Model):
         self.agents.shuffle_do("interact_do")
         self.datacollector.collect(self)
 
-        if self.datacollector.model_vars["consensus_reached"][-1] and self.params.early_stop:
+        if (
+            self.datacollector.model_vars["consensus_reached"][-1]
+            and self.params.early_stop
+        ):
             self.running = False
