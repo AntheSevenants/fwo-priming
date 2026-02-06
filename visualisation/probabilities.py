955ee0fa884665d39446e92bf731e9a79c995ceb
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-02-06 16:11:10 +0100

    Reinstate probabilities visualisation

---
+++ visualisation/probabilities.py
@@ -0,0 +1,28 @@
+import matplotlib.axes
+
+import model.model
+import visualisation.core
+
+
+def plot_ctx_probs_mean(model: model.model.PrimingModel,
+                        ax: matplotlib.axes.Axes = None,
+                        disable_title: bool = False):
+    return visualisation.core.plot_ratio(
+        model,
+        "ctx_probs_mean",
+        title="Mean probabilities per construction across agents",
+        ax=ax,
+        disable_title=disable_title,
+    )
+
+def plot_ctx_probs_per_agent(model: model.model.PrimingModel,
+                             disable_title: bool = False):
+    return visualisation.core.plot_ratio_pass(
+        model,
+        "ctx_probs_per_agent",
+        ylim=[0, 1],
+        baseline=0.5,
+        #secondary_baseline_attribute="starting_probs_per_agent",
+        title="Evolution of probabilities per agent",
+        disable_title=disable_title
+    )
