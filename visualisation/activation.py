1bd48cd083d8d8dbdc6fb59ea93310332b9c9740
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-02-06 15:52:11 +0100

    Rename visualisations pertaining to activation

--- visualisation/activation.py
+++ visualisation/activation.py
@@ -4,18 +4,18 @@ import model.model
 import visualisation.core
 
 
-def plot_ctx_probs_mean(model: model.model.PrimingModel,
+def plot_ctx_activation_mean(model: model.model.PrimingModel,
                         ax: matplotlib.axes.Axes = None,
                         disable_title: bool = False):
     return visualisation.core.plot_ratio(
         model,
         "ctx_probs_mean",
-        title="Mean probability across agents",
+        title="Mean activation per construction across agents",
         ax=ax,
         disable_title=disable_title,
     )
 
-def plot_ctx_probs_per_agent(model: model.model.PrimingModel,
+def plot_ctx_activation_per_agent(model: model.model.PrimingModel,
                              disable_title: bool = False):
     return visualisation.core.plot_ratio_pass(
         model,
@@ -23,6 +23,6 @@ def plot_ctx_probs_per_agent(model: model.model.PrimingModel,
         ylim=[0, 1],
         baseline=0.5,
         #secondary_baseline_attribute="starting_probs_per_agent",
-        title="Evolution of relative preference per agent",
+        title="Evolution of activation per agent",
         disable_title=disable_title
     )
