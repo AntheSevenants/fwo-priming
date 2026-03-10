e8e821a1960d4897ee6ecb6dacef25f33c8ba2af
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-03-10 10:58:02 +0100

    Add a lite present to prevent combinatorial explosion

--- batch/profiles.py
+++ batch/profiles.py
@@ -13,5 +13,18 @@ params = {
         "decay_to": model.enums.DecayTo.BASE_RATE,
         "affects_base_rate": model.enums.AffectsBaseRate.RECEPTION,
         "allow_decay_stop": False,
-    }
+    },
+    "lite": {
+        "num_agents": 10,
+        "starting_probabilities": [[0.1, 0.9], [0.2, 0.8], [0.4, 0.6], [0.5, 0.5]],
+        "priming_strength": 0.4,
+        "base_rate_change_strength": 0.05,
+        "inverse_frequency_exponent": 1,
+        "inverse_frequency_max_multiplier": 2,
+        "priming_opportunity": [ 0.05, 0.4 ],
+        "decay_strength": [0, 0.2, 0.5],
+        "decay_to": model.enums.DecayTo.BASE_RATE,
+        "affects_base_rate": model.enums.AffectsBaseRate.RECEPTION,
+        "allow_decay_stop": False,
+    },
 }
