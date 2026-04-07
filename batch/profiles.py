dc1ed14648fbf4d89e4a7fdcbed0d0be1e82cda8
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-04-07 14:58:10 +0200

    Update profiles

--- batch/profiles.py
+++ batch/profiles.py
@@ -3,14 +3,15 @@ import model.enums
 params = {
     "regular": {
         "num_agents": 10,
-        "starting_probabilities": [[0.1, 0.9], [0.2, 0.8], [0.4, 0.6], [0.5, 0.5]],
-        "priming_strength": [0.05, 0.2, 0.4, 0.5, 0.8],
-        "inverse_frequency_exponent": [0, 0.5, 1, 2],
-        "inverse_frequency_max_multiplier": 2,
-        "priming_opportunity": [0.001, 0.01, 0.05, 0.4],
-        "decay_strength": [0, 0.2, 0.5, 0.8],
+        "memory_size": [ 100, 1000 ],
+        "starting_probabilities": [[0.2, 0.8], [0.4, 0.6]],
+        "priming_strength": [0.05, 0.2, 0.4, 0.8],
+        "inverse_frequency_exponent": [0, 0.5, 1],
+        "inverse_frequency_max_multiplier": 4,
+        "priming_opportunity": [0.001, 0.01, 0.05, 0.4, 0.8],
+        "decay_strength": [0, 0.001, 0.01, 0.2, 0.5, 0.8],
         "decay_to": model.enums.DecayTo.BASE_RATE,
-        "affects_base_rate": model.enums.AffectsBaseRate.RECEPTION,
+        "affects_base_rate": [ model.enums.AffectsBaseRate.RECEPTION, model.enums.AffectsBaseRate.ALL ],
         "allow_decay_stop": False,
     },
     "lite": {
