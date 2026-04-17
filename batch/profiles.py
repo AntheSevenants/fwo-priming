4161bd3f766eda21c887159c848f3064f2f62935
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-04-17 15:50:32 +0200

    Update batch profiles

--- batch/profiles.py
+++ batch/profiles.py
@@ -3,17 +3,19 @@ import model.enums
 params = {
     "regular": {
         "num_agents": 10,
-        "memory_size": [ 100, 1000 ],
+        "memory_size": [ 1000 ],
         "starting_probabilities": [[0.2, 0.8], [0.4, 0.6]],
-        "priming_strength": [0.05, 0.2, 0.4, 0.8],
-        "inverse_frequency_exponent": [0, 0.5, 1],
+        "priming_strength": [0.05, 0.4, 2, 4],
+        # "inverse_frequency_exponent": [0, 0.5, 1],
         "inverse_frequency_max_multiplier": 4,
         "priming_opportunity": [0.001, 0.01, 0.05, 0.4, 0.8],
         "decay_strength": [0, 0.001, 0.01, 0.2, 0.5, 0.8],
         "decay_to": model.enums.DecayTo.BASE_RATE,
-        "affects_base_rate": [ model.enums.AffectsBaseRate.RECEPTION, model.enums.AffectsBaseRate.ALL ],
+        "affects_base_rate": [ model.enums.AffectsBaseRate.RECEPTION ],
         "allow_decay_stop": False,
         "activation_cap": False,
+        "base_rate_update_mechanism": model.enums.BaseRateUpdateMechanism.COUNT,
+        "logarithmic_perception": [True, False],
     },
     "lite": {
         "num_agents": 10,
@@ -27,7 +29,20 @@ params = {
         "affects_base_rate": model.enums.AffectsBaseRate.RECEPTION,
         "allow_decay_stop": False,
         "activation_cap": False,
+        "base_rate_update_mechanism": model.enums.BaseRateUpdateMechanism.COUNT,
     },
+}
+
+params["lite_latin"] = {
+    **params["lite"],
+    "base_rate_update_mechanism": model.enums.BaseRateUpdateMechanism.LATERAL_INHIBITION
+}
+
+params["lite_latin_log"] = {
+    **params["lite"],
+    "base_rate_update_mechanism": model.enums.BaseRateUpdateMechanism.LATERAL_INHIBITION,
+    "logarithmic_perception": True,
+    "inverse_frequency_exponent": 0
 }
 
 # Entrenchment profile, based on lite but with some params changed
@@ -36,4 +51,9 @@ params["entrenchment"] = {
     "decay_strength": 0,
     "use_activation": False,
     "inverse_frequency_exponent": 0
+}
+
+params["entrenchment_latin"] = {
+    **params["entrenchment"],
+    "base_rate_update_mechanism": model.enums.BaseRateUpdateMechanism.LATERAL_INHIBITION
 }
